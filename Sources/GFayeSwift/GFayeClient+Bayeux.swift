866a39ae6ae8fc78efeae2e2807d802f3607f989
Cindy Wong <ckpwong@gmail.com>
2020-07-28 11:31:50 -0400

    Fix for previous commit

--- Sources/GFayeSwift/GFayeClient+Bayeux.swift
+++ Sources/GFayeSwift/GFayeClient+Bayeux.swift
@@ -96,7 +96,7 @@ extension GFayeClient {
     // "supportedConnectionTypes": ["long-polling", "callback-polling", "iframe", "websocket]
     func handshake(allowedConnectionTypes: [BayeuxConnection], ext: [String:String] = [String:String]()) {
         writeOperationQueue.sync { [unowned self] in
-            let connTypes[String] = allowedConnectionTypes.map { $0.rawValue }
+            let connTypes = allowedConnectionTypes.map { $0.rawValue }
 
             var dict = [String: Any]()
             dict[Bayeux.channel.rawValue] = BayeuxChannel.handshake.rawValue
